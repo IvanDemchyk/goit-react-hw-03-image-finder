03693ec1c1a055de5af22ddea06072de229eaf75
IvanDemchyk <ivandem4ik@gmail.com>
2023-03-04 16:58:40 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { GlobalStyle } from './GlobalStyle';
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>React homework template</h1>
+        <GlobalStyle />
+      </div>
+    );
+  }
+}
